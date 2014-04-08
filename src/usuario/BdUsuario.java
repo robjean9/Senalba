f1040616d877b7df2861f0bc9321ed3b365f37da
barmybark <rafaelsktcod@gmail.com>
2014-04-08 13:49:36 -0300

    //02
    starting

---
+++ src/usuario/BdUsuario.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usuario;
+import java.sql.*;
+import javax.swing.JOptionPane;
+import vo.usuario;
+public class BdUsuario extends bd.Bd{
+    public BdUsuario(){
+        try{
+            conexao();
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Erro: " +e.getMessage());
+        }
+    }
+    public void insere(usuario usuario){
+        String sql = "insert into usuario (nome, email, tipo, login, senha) values(?,?,?,?,?)";
+        try{
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getEmail());
+            ps.setString(3, usuario.getTipo());
+            ps.setString(4, usuario.getLogin());
+            ps.setString(5, usuario.getSenha());
+            ps.execute();
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro SQL:" +e.getMessage());
+        }
+    }
+    
+    public void atualiza(usuario usuario){
+        String sql = "update usuario set nome=?, email=?, tipo=?, login=?, senha=? where codigo=?";
+        try{
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getEmail());
+            ps.setString(3, usuario.getTipo());
+            ps.setString(4, usuario.getLogin());
+            ps.setString(5, usuario.getSenha());
+            ps.setInt(6, usuario.getCodigo());
+            ps.execute();
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro SQL:" +e.getMessage());
+        }
+        
+        
+    }
+    
+    public usuario localiza(String login, String senha){
+        //tipo='" + tipo + "'
+        String sql = "select login,senha,tipo from usuario where login='" + login + "' and senha='" + senha + "'";
+        vo.usuario registro = new usuario();
+        try{
+            Statement st = getCon().createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            if(rs.next()){
+                registro.setLogin(rs.getString("login"));
+                registro.setSenha(rs.getString("senha"));
+                registro.setTipo(rs.getString("tipo"));
+                
+            }
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro SQL:" +e.getMessage());
+        }
+        
+        return registro;
+    }
+    public usuario localizaMail(String email){
+        String sql = "select email,login,senha from usuario where email = '" + email + "'";
+        vo.usuario registro = new usuario();
+        try{
+            Statement st = getCon().createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            if(rs.next()){
+                registro.setEmail(rs.getString("email"));
+                registro.setLogin(rs.getString("login"));
+                registro.setSenha(rs.getString("senha"));
+            }
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro SQL:" +e.getMessage());
+        }
+        
+        return registro;
+    }
+    
+    /*public String localizaTipo(String login, String tipo){
+        String sql = "select login,tipo from usuario wheretipo = '" + tipo + "'";
+        //vo.usuario registro = new usuario();
+        try{
+            Statement st = getCon().createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            if(rs.next()){
+                registro.setTipo(rs.getString("tipo"));
+            }
+        }catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro SQL:" +e.getMessage());
+        }
+        
+        return registro;
+    }*/
+
+   
+
+   
+ 
+
+
+    
+   
+        
+    }
+
---
+++ src/usuario/usuario.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usuario;
+
+/**
+ *
+ * @author rafael
+ */
+public class usuario {
+    private int codigo;
+    private String nome;
+    private String email;
+    public String tipo;
+    private String login;
+    private String senha;
+
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
