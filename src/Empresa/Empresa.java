3521582439f7222f6b8ebfd9b90619576eedf65d
welingtonjs <welingtonsieklicki@gmail.com>
2014-04-11 17:23:57 -0300

    #11

---
+++ src/Empresa/Empresa.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Empresa;
+
+/**
+ *
+ * @author Welington
+ */
+public class Empresa {
+    private Integer Codigo;
+    private String Nome;
+    private String CNPJ;
+    private String InscricaoEstadual;
+    private String CNAE;
+
+    /**
+     * @return the Codigo
+     */
+    public Integer getCodigo() {
+        return Codigo;
+    }
+
+    /**
+     * @param Codigo the Codigo to set
+     */
+    public void setCodigo(Integer Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    /**
+     * @return the Nome
+     */
+    public String getNome() {
+        return Nome;
+    }
+
+    /**
+     * @param Nome the Nome to set
+     */
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    /**
+     * @return the CNPJ
+     */
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    /**
+     * @param CNPJ the CNPJ to set
+     */
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    /**
+     * @return the InscricaoEstadual
+     */
+    public String getInscricaoEstadual() {
+        return InscricaoEstadual;
+    }
+
+    /**
+     * @param InscricaoEstadual the InscricaoEstadual to set
+     */
+    public void setInscricaoEstadual(String InscricaoEstadual) {
+        this.InscricaoEstadual = InscricaoEstadual;
+    }
+
+    /**
+     * @return the CNAE
+     */
+    public String getCNAE() {
+        return CNAE;
+    }
+
+    /**
+     * @param CNAE the CNAE to set
+     */
+    public void setCNAE(String CNAE) {
+        this.CNAE = CNAE;
+    }
+
+    /**
+     * @return the Codigo
+     */
+}
